d50d218ba1bc07b81248fd0ad77dc7bba230888d
matthew seidel <mseidel@misena.edu.co>
2023-07-12 21:59:19 -0500

    delete unused routes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,19 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomeView } from "../views/HomeView";
 
-import { LoginView } from "../views/LoginView";
+// import { LoginView } from "../views/LoginView";
 
-import { RegisterView } from "../views/RegisterView";
-
+// import { RegisterView } from "../views/RegisterView";
 export const routes = createBrowserRouter([
-  {
-    path: "/login",
-    element: <LoginView />,
-  },
-  {
-    path: "/register",
-    element: <RegisterView />,
-  },
+  // {
+  //   path: "/login",
+  //   element: <LoginView />,
+  // },
+  // {
+  //   path: "/register",
+  //   element: <RegisterView />,
+  // },
   {
     path: "/favorites",
     element: <HomeView />,
